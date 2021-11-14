dda42698a8cd01df1cad1c4ecd4adae8fedee3d8
Shubham Mule <shubhambmule@gmail.com>
2021-11-14 16:53:02 +0530

    Added : Retrieve payroll from database

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ namespace EmpolyeePayrollService
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Payroll Service");
+            EmployeeRepository repository = new EmployeeRepository();
+            Console.WriteLine("Id Name BasicPay Startdate Gender Department PhoneNumber Address Deduction Tax IncomeTax NetPay \n");
+            repository.GetAllEmployee();
+            Console.ReadLine();
         }
     }
 }
