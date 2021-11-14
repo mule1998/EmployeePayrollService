837f42f2f7c7d20762541404f146f2fa96ed05b4
Shubham Mule <shubhambmule@gmail.com>
2021-11-14 16:40:47 +0530

    Added : Database connection to VS

---
+++ EmployeeModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpolyeePayrollService
+{
+    class EmployeeModel
+    {
+        //Getter and Setter fields(present in db)
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public double PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+        public string Gender { get; set; }
+        public double BasicPay { get; set; }
+        public double Deductions { get; set; }
+        public double TaxablePay { get; set; }
+        public double IncomeTax { get; set; }
+        public double NetPay { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
---
+++ EmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace EmpolyeePayrollService
+{
+    class EmployeeRepository
+    {
+        //Connection to SQL Database
+        public static string connectionString = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = payroll_serviceADO; Integrated Security = True";
+
+        //passing the string to sqlconnection to make connection 
+        SqlConnection sqlconnection = new SqlConnection(connectionString);
+    }
+}
